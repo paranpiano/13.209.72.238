23d3bff13ab0e03a8cb470d5ea7a908fb656506a
uidg3913 <uidg3913@ICL2141W>
2018-07-23 17:08:27 +0900

    Passing Python Variables to Be Rendered in the Template

--- lists/tests.py
+++ lists/tests.py
@@ -19,3 +19,9 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html')  
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        print('response.content = {}'.format(response.content.decode()))
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
